93e603a7cfb516ec874bffd5b072f5817f1a99d6
Tim <tim.a.ung.12#gmail.com>
2023-09-04 22:34:54 +1000

    players can now start the game

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 let cardOne = 5
 let cardTwo = 6
-
 let sum = cardOne + cardTwo
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function startGame() {
+    sumEl.textContent += sum
+    cardsEl.textContent += cardOne + " | " + cardTwo
+
+    if (sum < 21) {
+        message = "Do you want to draw another card?"
+    } else if (sum === 21) {
+        message = "WooHoo! You've got BlackJack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+}
+
+function newCard() {
+    
+}
