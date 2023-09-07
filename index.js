1fd0396bf0ca544c3a5bf87d9daab0f59493f3b2
Tim <tim.a.ung.12#gmail.com>
2023-09-07 17:19:10 +1000

    completed basic blackjack game

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-const cardsInHand = [getRandomCard(), getRandomCard()]
-let sum = cardsInHand.reduce(function(x,y) {
-    return x+y;
-}, 0)
+const cardsInHand = []
+let sum;
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name : "Tim",
+    chips : 145
+}
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function startGame() {
+    isAlive = true
+
+    for (i = 0; i < 2; i++) {
+        cardsInHand.push(getRandomCard())
+    }
+
+    sum = cardsInHand.reduce(function(x,y) {
+        return x+y;
+    }, 0)
+
     renderGame()
 }
 
 function getRandomCard() {
-    return Math.floor(Math.random() * 21) + 1
+    let randomCardValue = Math.floor(Math.random() * 13) + 1
+
+    if (randomCardValue === 1) {
+        return 11
+    } else if (randomCardValue > 10 && randomCardValue <= 13) {
+        return 10
+    } else {
+        return randomCardValue
+    }
 }
 
 function renderGame() {
@@ -35,9 +60,10 @@ function renderGame() {
 }
 
 function newCard() {
-    let drawnCard = getRandomCard()
-    cardsInHand.push(drawnCard)
-    sum += drawnCard
-
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let drawnCard = getRandomCard()
+        cardsInHand.push(drawnCard)
+        sum += drawnCard
+        renderGame()
+    }
 }
